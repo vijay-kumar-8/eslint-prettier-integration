a64cbdd5e4b7301a0925c69bec0f5b1e08e7f5ec
vijay-kumar-8 <vijisnv97@gmail.com>
2025-03-19 12:44:02 +0530

    check lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core'
 import { ApiService } from './api.service'
-import { pipeline } from '@xenova/transformers'
+// import { pipeline } from '@xenova/transformers'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,28 +16,28 @@ export class AppComponent {
   //   const greeting = 'Hello, world!';
   // }
 
-  inputText: string = ''
-  inputText1: string = ''
-  generatedText: string = ''
-  translatedText: string = ''
-  userText: string = ''
+  inputText = ''
+  inputText1 = ''
+  generatedText = ''
+  translatedText = ''
+  userText = ''
   useCases: string[] = []
-  isLoading: boolean = false
-  summarizedText: string = ''
+  isLoading = false
+  summarizedText = ''
 
   constructor(private huggingfaceService: ApiService) {}
 
   // Method to call Hugging Face API
-  onGenerateText(): void {
-    this.huggingfaceService.getTextGeneration(this.inputText1).subscribe(
-      (response: { generated_text: string }[]) => {
-        this.generatedText = response[0].generated_text
-      },
-      (error: any) => {
-        console.error('Error:', error)
-      },
-    )
-  }
+  // onGenerateText(): void {
+  //   this.huggingfaceService.getTextGeneration(this.inputText1).subscribe(
+  //     (response: { generated_text: string }[]) => {
+  //       this.generatedText = response[0].generated_text
+  //     },
+  //     (error: any) => {
+  //       console.error('Error:', error)
+  //     },
+  //   )
+  // }
 
   // onSubmit() {
   //   this.huggingfaceService.translateText(this.inputText).subscribe(
@@ -53,17 +53,17 @@ export class AppComponent {
   //   );
   // }
 
-  onSubmit() {
-    this.huggingfaceService.summarizeText(this.inputText).subscribe(
-      (response: any) => {
-        // Assuming the summarized text is returned under the 'summary_text' key
-        this.summarizedText =
-          response[0]?.generated_text || 'No summary available.'
-      },
-      (error: any) => {
-        console.error(error)
-        this.summarizedText = 'Error occurred while summarizing.'
-      },
-    )
-  }
+  // onSubmit() {
+  //   this.huggingfaceService.summarizeText(this.inputText).subscribe(
+  //     (response: any) => {
+  //       // Assuming the summarized text is returned under the 'summary_text' key
+  //       this.summarizedText =
+  //         response[0]?.generated_text || 'No summary available.'
+  //     },
+  //     (error: any) => {
+  //       console.error(error)
+  //       this.summarizedText = 'Error occurred while summarizing.'
+  //     },
+  //   )
+  // }
 }
